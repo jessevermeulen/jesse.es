6866138b6becfd19785dd7c13ca973ff4ee44b13
Jesse Vermeulen <jesse.vermeulen3@hva.nl>
2022-02-04 16:32:08 +0100

    Add Logo component

---
+++ src/lib/components/Logo.svelte
@@ -0,0 +1,84 @@
+<svg width="64" height="64" viewBox="0 0 64 64" fill="none" xmlns="http://www.w3.org/2000/svg">
+	<g clip-path="url(#clip0_516_35)">
+		<rect width="64" height="64" rx="14.6019" fill="white" />
+		<g filter="url(#filter0_ii_516_35)">
+			<rect x="-0.0307617" width="64" height="64" rx="14.6019" fill="url(#paint0_linear_516_35)" />
+		</g>
+		<g style="mix-blend-mode:overlay">
+			<rect
+				x="0.969238"
+				y="1"
+				width="62"
+				height="62"
+				rx="13.6019"
+				stroke="white"
+				stroke-width="2"
+			/>
+		</g>
+		<path
+			fill-rule="evenodd"
+			clip-rule="evenodd"
+			d="M46.3245 16.0518V22.2797L31.8261 42.5775V47.9482H29.1511V41.7202L43.6495 21.4225V16.0518H46.3245Z"
+			fill="white"
+		/>
+		<path
+			fill-rule="evenodd"
+			clip-rule="evenodd"
+			d="M17.5525 16.0518H34.7258V22.2797L20.2274 42.5775V47.9482H17.5525V41.7202L32.0509 21.4225V18.7267H17.5525V16.0518Z"
+			fill="white"
+		/>
+	</g>
+	<defs>
+		<filter
+			id="filter0_ii_516_35"
+			x="-0.0307617"
+			y="-4.03884"
+			width="64"
+			height="72.0777"
+			filterUnits="userSpaceOnUse"
+			color-interpolation-filters="sRGB"
+		>
+			<feFlood flood-opacity="0" result="BackgroundImageFix" />
+			<feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+			<feColorMatrix
+				in="SourceAlpha"
+				type="matrix"
+				values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+				result="hardAlpha"
+			/>
+			<feOffset dy="4.03884" />
+			<feGaussianBlur stdDeviation="2.01942" />
+			<feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1" />
+			<feColorMatrix type="matrix" values="0 0 0 0 0.218353 0 0 0 0 0.300235 0 0 0 0 1 0 0 0 1 0" />
+			<feBlend mode="normal" in2="shape" result="effect1_innerShadow_516_35" />
+			<feColorMatrix
+				in="SourceAlpha"
+				type="matrix"
+				values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+				result="hardAlpha"
+			/>
+			<feOffset dy="-4.03884" />
+			<feGaussianBlur stdDeviation="2.01942" />
+			<feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1" />
+			<feColorMatrix
+				type="matrix"
+				values="0 0 0 0 0.150588 0 0 0 0 0.207059 0 0 0 0 0.8 0 0 0 1 0"
+			/>
+			<feBlend mode="normal" in2="effect1_innerShadow_516_35" result="effect2_innerShadow_516_35" />
+		</filter>
+		<linearGradient
+			id="paint0_linear_516_35"
+			x1="31.9692"
+			y1="0"
+			x2="31.9692"
+			y2="64"
+			gradientUnits="userSpaceOnUse"
+		>
+			<stop stop-color="#364AFF" />
+			<stop offset="1" stop-color="#2837D6" />
+		</linearGradient>
+		<clipPath id="clip0_516_35">
+			<rect width="64" height="64" rx="14.6019" fill="white" />
+		</clipPath>
+	</defs>
+</svg>
