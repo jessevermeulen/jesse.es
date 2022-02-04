d58440d7c9d524b9c8d2980b9397930c0747129c
Jesse Vermeulen <jesse.vermeulen3@hva.nl>
2022-02-04 16:33:00 +0100

    Stylistic changes

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,17 +1,17 @@
 <script>
+	import Logo from '$lib/components/Logo.svelte';
 	import Redirects from '$lib/content/redirects.svelte.md';
 </script>
 
-<div class="grid gap-8 max-w-md m-auto pt-32">
-	<!-- <header class="grid gap-6 after:bg-gray-700 after:h-px after:rounded-[0.5px]"> -->
+<div class="grid gap-8 sm:max-w-md m-auto py-16 sm:py-32 px-4 sm:px-0">
 	<header class="grid gap-6">
-		<img src="/jve-icon.svg" alt="Jesse Vermeulen logomark" />
+		<Logo />
 
 		<div class="grid gap-3 pt-6">
 			<h1 class="text-xl font-semibold">Jesse Vermeulen</h1>
 			<p class="text-lg">
 				Amsterdam based Digital Product Designer with over 8 years of experience in both design and
-				development
+				development.
 			</p>
 		</div>
 	</header>
