d1cba72cce13e2ff68077eac41a7bb1543fbead7
Jesse Vermeulen <jesse.vermeulen3@hva.nl>
2022-02-05 15:01:36 +0100

    Update logo

--- src/lib/components/Logo.svelte
+++ src/lib/components/Logo.svelte
@@ -1,8 +1,11 @@
 <svg width="64" height="64" viewBox="0 0 64 64" fill="none" xmlns="http://www.w3.org/2000/svg">
-	<g clip-path="url(#clip0_516_35)">
-		<rect width="64" height="64" rx="14.6019" fill="white" />
-		<g filter="url(#filter0_ii_516_35)">
-			<rect x="-0.0307617" width="64" height="64" rx="14.6019" fill="url(#paint0_linear_516_35)" />
+	<g clip-path="url(#clip0_553_98)">
+		<path
+			d="M0 14.6019C0 6.53751 6.53751 0 14.6019 0H49.3981C57.4625 0 64 6.53751 64 14.6019V49.3981C64 57.4625 57.4625 64 49.3981 64H14.6019C6.53751 64 0 57.4625 0 49.3981V14.6019Z"
+			fill="white"
+		/>
+		<g filter="url(#filter0_ii_553_98)">
+			<rect x="-0.0307617" width="64" height="64" rx="14.6019" fill="url(#paint0_linear_553_98)" />
 		</g>
 		<g style="mix-blend-mode:overlay">
 			<rect
@@ -15,22 +18,15 @@
 				stroke-width="2"
 			/>
 		</g>
+		<path d="M30.4888 47.9483V42.1489L44.9872 21.8512V16.0518" stroke="white" stroke-width="3" />
 		<path
-			fill-rule="evenodd"
-			clip-rule="evenodd"
-			d="M46.3245 16.0518V22.2797L31.8261 42.5775V47.9482H29.1511V41.7202L43.6495 21.4225V16.0518H46.3245Z"
-			fill="white"
-		/>
-		<path
-			fill-rule="evenodd"
-			clip-rule="evenodd"
-			d="M17.5525 16.0518H34.7258V22.2797L20.2274 42.5775V47.9482H17.5525V41.7202L32.0509 21.4225V18.7267H17.5525V16.0518Z"
+			d="M18.8899 42.1489L17.6693 41.277L17.3899 41.6682V42.1489H18.8899ZM33.3883 21.8512L34.6089 22.723L34.8883 22.3319V21.8512H33.3883ZM33.3883 17.3893H34.8883V15.8893H33.3883V17.3893ZM18.8899 15.8893H17.3899V18.8893H18.8899V15.8893ZM20.3899 47.9483V42.1489H17.3899V47.9483H20.3899ZM20.1105 43.0208L34.6089 22.723L32.1677 20.9793L17.6693 41.277L20.1105 43.0208ZM34.8883 21.8512V17.3893H31.8883V21.8512H34.8883ZM33.3883 15.8893H18.8899V18.8893H33.3883V15.8893Z"
 			fill="white"
 		/>
 	</g>
 	<defs>
 		<filter
-			id="filter0_ii_516_35"
+			id="filter0_ii_553_98"
 			x="-0.0307617"
 			y="-4.03884"
 			width="64"
@@ -50,7 +46,7 @@
 			<feGaussianBlur stdDeviation="2.01942" />
 			<feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1" />
 			<feColorMatrix type="matrix" values="0 0 0 0 0.218353 0 0 0 0 0.300235 0 0 0 0 1 0 0 0 1 0" />
-			<feBlend mode="normal" in2="shape" result="effect1_innerShadow_516_35" />
+			<feBlend mode="normal" in2="shape" result="effect1_innerShadow_553_98" />
 			<feColorMatrix
 				in="SourceAlpha"
 				type="matrix"
@@ -64,10 +60,10 @@
 				type="matrix"
 				values="0 0 0 0 0.150588 0 0 0 0 0.207059 0 0 0 0 0.8 0 0 0 1 0"
 			/>
-			<feBlend mode="normal" in2="effect1_innerShadow_516_35" result="effect2_innerShadow_516_35" />
+			<feBlend mode="normal" in2="effect1_innerShadow_553_98" result="effect2_innerShadow_553_98" />
 		</filter>
 		<linearGradient
-			id="paint0_linear_516_35"
+			id="paint0_linear_553_98"
 			x1="31.9692"
 			y1="0"
 			x2="31.9692"
@@ -77,8 +73,11 @@
 			<stop stop-color="#364AFF" />
 			<stop offset="1" stop-color="#2837D6" />
 		</linearGradient>
-		<clipPath id="clip0_516_35">
-			<rect width="64" height="64" rx="14.6019" fill="white" />
+		<clipPath id="clip0_553_98">
+			<path
+				d="M0 14.6019C0 6.53751 6.53751 0 14.6019 0H49.3981C57.4625 0 64 6.53751 64 14.6019V49.3981C64 57.4625 57.4625 64 49.3981 64H14.6019C6.53751 64 0 57.4625 0 49.3981V14.6019Z"
+				fill="white"
+			/>
 		</clipPath>
 	</defs>
 </svg>
