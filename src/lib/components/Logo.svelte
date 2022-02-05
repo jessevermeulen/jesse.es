92e950d5a12fecde97b451e186e1f794fefa3312
Jesse Vermeulen <jesse.vermeulen3@hva.nl>
2022-02-05 17:12:26 +0100

    Update logo

--- src/lib/components/Logo.svelte
+++ src/lib/components/Logo.svelte
@@ -1,15 +1,15 @@
 <svg width="64" height="64" viewBox="0 0 64 64" fill="none" xmlns="http://www.w3.org/2000/svg">
-	<g clip-path="url(#clip0_553_98)">
+	<g clip-path="url(#clip0_563_130)">
 		<path
 			d="M0 14.6019C0 6.53751 6.53751 0 14.6019 0H49.3981C57.4625 0 64 6.53751 64 14.6019V49.3981C64 57.4625 57.4625 64 49.3981 64H14.6019C6.53751 64 0 57.4625 0 49.3981V14.6019Z"
 			fill="white"
 		/>
-		<g filter="url(#filter0_ii_553_98)">
-			<rect x="-0.0307617" width="64" height="64" rx="14.6019" fill="url(#paint0_linear_553_98)" />
+		<g filter="url(#filter0_ii_563_130)">
+			<rect x="-0.0306988" width="64" height="64" rx="14.6019" fill="url(#paint0_linear_563_130)" />
 		</g>
 		<g style="mix-blend-mode:overlay">
 			<rect
-				x="0.969238"
+				x="0.969301"
 				y="1"
 				width="62"
 				height="62"
@@ -18,16 +18,15 @@
 				stroke-width="2"
 			/>
 		</g>
-		<path d="M30.5503 47.9483V42.1489L45.0487 21.8512V16.0518" stroke="white" stroke-width="3" />
 		<path
-			d="M18.9514 42.1489L17.7308 41.277L17.4514 41.6682V42.1489H18.9514ZM33.4498 21.8512L34.6704 22.723L34.9498 22.3319V21.8512H33.4498ZM33.4498 17.3893H34.9498V15.8893H33.4498V17.3893ZM18.9514 15.8893H17.4514V18.8893H18.9514V15.8893ZM20.4514 47.9483V42.1489H17.4514V47.9483H20.4514ZM20.172 43.0208L34.6704 22.723L32.2292 20.9793L17.7308 41.277L20.172 43.0208ZM34.9498 21.8512V17.3893H31.9498V21.8512H34.9498ZM33.4498 15.8893H18.9514V18.8893H33.4498V15.8893Z"
+			d="M32 51.4399L50 15.4399V33.0177C50 37.03 47.9948 40.7768 44.6564 43.0023L32 51.4399L19.3436 43.0023C16.0052 40.7767 14 37.03 14 33.0177V15.4399L32 27.4399V51.4399Z"
 			fill="white"
 		/>
 	</g>
 	<defs>
 		<filter
-			id="filter0_ii_553_98"
-			x="-0.0307617"
+			id="filter0_ii_563_130"
+			x="-0.0306988"
 			y="-4.03884"
 			width="64"
 			height="72.0777"
@@ -46,7 +45,7 @@
 			<feGaussianBlur stdDeviation="2.01942" />
 			<feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1" />
 			<feColorMatrix type="matrix" values="0 0 0 0 0.218353 0 0 0 0 0.300235 0 0 0 0 1 0 0 0 1 0" />
-			<feBlend mode="normal" in2="shape" result="effect1_innerShadow_553_98" />
+			<feBlend mode="normal" in2="shape" result="effect1_innerShadow_563_130" />
 			<feColorMatrix
 				in="SourceAlpha"
 				type="matrix"
@@ -60,20 +59,24 @@
 				type="matrix"
 				values="0 0 0 0 0.150588 0 0 0 0 0.207059 0 0 0 0 0.8 0 0 0 1 0"
 			/>
-			<feBlend mode="normal" in2="effect1_innerShadow_553_98" result="effect2_innerShadow_553_98" />
+			<feBlend
+				mode="normal"
+				in2="effect1_innerShadow_563_130"
+				result="effect2_innerShadow_563_130"
+			/>
 		</filter>
 		<linearGradient
-			id="paint0_linear_553_98"
-			x1="31.9692"
+			id="paint0_linear_563_130"
+			x1="31.9693"
 			y1="0"
-			x2="31.9692"
+			x2="31.9693"
 			y2="64"
 			gradientUnits="userSpaceOnUse"
 		>
 			<stop stop-color="#364AFF" />
 			<stop offset="1" stop-color="#2837D6" />
 		</linearGradient>
-		<clipPath id="clip0_553_98">
+		<clipPath id="clip0_563_130">
 			<path
 				d="M0 14.6019C0 6.53751 6.53751 0 14.6019 0H49.3981C57.4625 0 64 6.53751 64 14.6019V49.3981C64 57.4625 57.4625 64 49.3981 64H14.6019C6.53751 64 0 57.4625 0 49.3981V14.6019Z"
 				fill="white"
